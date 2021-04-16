78f4d4bd484faea7e98a6a5dea1aac00c247a0e8
chandapwes <67353485+chandapwes@users.noreply.github.com>
2021-04-16 10:36:08 -0600

    what I figured out

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+// import POTD from './components/POTD';
 
 function App() {
+  const [photo, setPhoto] = useState(null)
+  // const [date, setDate] = useState(null)
+
+  useEffect(function(){
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(function(res){
+      setPhoto(res.photo)
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+  }, [])
+
+  console.log(photo)
+
   return (
     <div className="App">
       <p>
